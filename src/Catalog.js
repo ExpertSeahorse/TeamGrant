52f054183553bb23d0a056aa5bc6443ef85dcce2
David <dtfeldman2669@gmail.com>
2021-03-30 23:17:05 -0400

    moved the sort logic outside the Catalog

--- src/Catalog.js
+++ src/Catalog.js
@@ -1,105 +1,71 @@
-import React from 'react'
-//import { render } from 'react-dom'
-import CatalogItem from './CatalogItem'
+import useSortableData from './SortAlgo'
 
-// Create sorted data
-const useSortableData = (items, config = null) => {
-  // sortConfig remembers the current sort pattern
-  const [sortConfig, setSortConfig] = React.useState(config);
-  
-  // memoize the sorting algo so if the table is rerendered the sort is not recalculated
-  const sortedItems = React.useMemo(() => {
-    // copy the items arr to maintain the orig
-    let sortableItems = [...items];
-    // if sortConfig exists:
-    if (sortConfig !== null) {
-      // sort the table by a's key and b's key and direction. key + direction are set in requestSort
-      sortableItems.sort((a, b) => {
-        if (a[sortConfig.key] < b[sortConfig.key]) {
-          return sortConfig.direction === 'ascending' ? -1 : 1;
-        }
-        if (a[sortConfig.key] > b[sortConfig.key]) {
-          return sortConfig.direction === 'ascending' ? 1 : -1;
-        }
-        return 0;
-      });
-    }
-    return sortableItems;
-  }, [items, sortConfig]);
-
-  // set the value of the config with input from button 
-  const requestSort = key => {
-    let direction = 'ascending';
-    // if already ascending and previous sort was the same key, swap to decending
-    if (
-      sortConfig && 
-      sortConfig.key === key && 
-      sortConfig.direction === 'ascending'
-    ) {
-      direction = 'descending';
-    }
-    // set SortConfig to key, direction
-    setSortConfig({ key, direction });
-  }
-
-  return { items: sortedItems, requestSort, sortConfig  };
+// creates a row in the Catalog table, assuming the catalog is passed as an Object.entities(dict)
+function CatalogItem(props) {
+  return (
+      <tr key={props.value[0]}>
+        <td>{props.value[0]}</td>
+        <td>{props.value[1].itemType}</td>
+        <td>{props.value[1].color}</td>
+      </tr>
+  )
 }
 
 //input props: array
 const CatalogTable = (props) => {
-  // get data + sort it w/ above process
-  const { items, requestSort, sortConfig } = useSortableData(props.products);
-  
-  // for styling later
-  const getClassNamesFor = (name) => {
-    if (!sortConfig) {
-      return;
-    }
-    return sortConfig.key === name ? sortConfig.direction : undefined;
-  };
-  
-  return (
-    <table>
-      <caption>Catalog</caption>
-      <thead>
-        <tr>
-          <th>
-            {/* Table Headers */}
-            ID
-            <button
-              type="button" 
-              onClick={() => requestSort('id')}
-              className={getClassNamesFor('id')}>
-              {getClassNamesFor('id')}
-            </button>
-          </th>
-          <th>
-            Item Type
-            <button 
-            type="button" 
-            onClick={() => requestSort('itemType')}
-            className={getClassNamesFor('itemType')}>
-            {getClassNamesFor('itemType')}
-            </button>
-          </th>
-          <th>
-            Color
-            <button 
-            type="button" 
-            onClick={() => requestSort('color')}
-            className={getClassNamesFor('color')}>
-            {getClassNamesFor('color')}
-            </button>
-          </th>
-        </tr>
-      </thead>
-      <tbody>{ 
-        // Loop over the sorted list and make a row in the Table for each item
-        items.map(p => <CatalogItem value={p}/> )
+    // get data + sort it w/ above process
+    const { items, requestSort, sortConfig } = useSortableData(props.products);
+
+    // for styling later
+    const getClassNamesFor = (name) => {
+        if (!sortConfig) {
+            return;
         }
-      </tbody>
-    </table>
-  );
+        return sortConfig.key === name ? sortConfig.direction : undefined;
+    };
+
+    return (
+        <table>
+            <caption>Catalog</caption>
+            <thead>
+                <tr key={"Catalog Header"}>
+                    <th>
+                        {/* Table Headers */}
+                        ID
+                        <button
+                        type="button" 
+                        onClick={() => requestSort('id')}
+                        className={getClassNamesFor('id')}>
+                        {getClassNamesFor('id')}
+                        </button>
+                    </th>
+                    <th>
+                        Item Type
+                        <button 
+                        type="button" 
+                        onClick={() => requestSort('itemType')}
+                        className={getClassNamesFor('itemType')}>
+                        {getClassNamesFor('itemType')}
+                        </button>
+                    </th>
+                    <th>
+                        Color
+                        <button 
+                        type="button" 
+                        onClick={() => requestSort('color')}
+                        className={getClassNamesFor('color')}>
+                        {getClassNamesFor('color')}
+                        </button>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>{ 
+                // Loop over the sorted list and make a row in the Table for each item
+                items.map(p => <CatalogItem value={p}/> )
+            }
+            </tbody>
+        </table>
+    );
 }
 
 export default function Catalog() {
--- src/ItemList.js
+++ src/ItemList.js
@@ -3,12 +3,13 @@ import React from 'react'
 // Builds the Inventory table in HTML by converting a table of IDs to a table of pieces of furnisher
 export default function ItemList({ items }) {
   const catalog = require("./data/catalog.json")
+  let i = 0
   return (
     items.map(item => {
       if(catalog[item.id] === undefined) return
       else
         return(
-          <tr>
+          <tr key={i++}>
             <td>{catalog[item.id].itemType}</td>
             <td>{catalog[item.id].color}</td>
           </tr>
