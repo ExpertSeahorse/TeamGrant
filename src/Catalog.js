4e0936e4f95c654c0ffca8c84a40ec7818a3a598
David <dtfeldman2669@gmail.com>
2021-03-31 17:43:13 -0400

    fixed Catalog not sorting

--- src/Catalog.js
+++ src/Catalog.js
@@ -3,45 +3,45 @@ import React from 'react'
 
 // Create sorted data
 const useSortableData = (items, config = null) => {
-    // sortConfig remembers the current sort pattern
-    const [sortConfig, setSortConfig] = React.useState(config);
+    // CatalogSortConfig remembers the current sort pattern
+    const [CatalogSortConfig, setCatalogSortConfig] = React.useState(config);
     
     // memoize the sorting algo so if the table is rerendered the sort is not recalculated
     const sortedItems = React.useMemo(() => {
       // copy the items arr to maintain the orig
       let sortableItems = [...items];
-      // if sortConfig exists:
-      if (sortConfig !== null) {
+      // if CatalogSortConfig exists:
+      if (CatalogSortConfig !== null) {
         // sort the table by a's key and b's key and direction. key + direction are set in requestSort
         sortableItems.sort((a, b) => {
-          if (a[sortConfig.key] < b[sortConfig.key]) {
-            return sortConfig.direction === 'ascending' ? -1 : 1;
+          if (a[CatalogSortConfig.key] < b[CatalogSortConfig.key]) {
+            return CatalogSortConfig.direction === 'ascending' ? -1 : 1;
           }
-          if (a[sortConfig.key] > b[sortConfig.key]) {
-            return sortConfig.direction === 'ascending' ? 1 : -1;
+          if (a[CatalogSortConfig.key] > b[CatalogSortConfig.key]) {
+            return CatalogSortConfig.direction === 'ascending' ? 1 : -1;
           }
           return 0;
         });
       }
       return sortableItems;
-    }, [items, sortConfig]);
+    }, [items, CatalogSortConfig]);
   
     // set the value of the config with input from button 
     const requestSort = key => {
       let direction = 'ascending';
       // if already ascending and previous sort was the same key, swap to decending
       if (
-        sortConfig && 
-        sortConfig.key === key && 
-        sortConfig.direction === 'ascending'
+        CatalogSortConfig && 
+        CatalogSortConfig.key === key && 
+        CatalogSortConfig.direction === 'ascending'
       ) {
         direction = 'descending';
       }
-      // set SortConfig to key, direction
-      setSortConfig({ key, direction });
+      // set CatalogSortConfig to key, direction
+      setCatalogSortConfig({ key, direction });
     }
   
-    return { items: sortedItems, requestSort, sortConfig  };
+    return { items: sortedItems, requestSort, CatalogSortConfig  };
 }
 // creates a row in the Catalog table, assuming the catalog is passed as an Object.entities(dict)
 /* function CatalogItem(p) {
@@ -58,14 +58,14 @@ const useSortableData = (items, config = null) => {
 //input props: array
 const CatalogTable = (props) => {
     // get data + sort it w/ above process
-    const { items, requestSort, sortConfig } = useSortableData(props.products);
+    const { items, requestSort, CatalogSortConfig } = useSortableData(props.products);
 
     // for styling later
     const getClassNamesFor = (name) => {
-        if (!sortConfig) {
+        if (!CatalogSortConfig) {
             return;
         }
-        return sortConfig.key === name ? sortConfig.direction : undefined;
+        return CatalogSortConfig.key === name ? CatalogSortConfig.direction : undefined;
     };
 
     return (
@@ -105,10 +105,10 @@ const CatalogTable = (props) => {
             </thead>
             <tbody>{ // Loop over the sorted list and make a row in the Table for each item
               items.map(p => (
-                <tr key={p[0]}>
-                  <td>{p[0]}</td>
-                  <td>{p[1].itemType}</td>
-                  <td>{p[1].color}</td>
+                <tr key={p.id}>
+                  <td>{p.id}</td>
+                  <td>{p.itemType}</td>
+                  <td>{p.color}</td>
                 </tr>
               ))
             }
@@ -119,7 +119,18 @@ const CatalogTable = (props) => {
 
 export default function Catalog() {
   const catalog = require('./data/catalog.json')
-  console.log(Object.entries(catalog))
+  const cata = Object.entries(catalog)
+  console.log(cata)
+  let prods = []
+  cata.forEach(p => {
+    prods.push({
+      id: p[0],
+      itemType: p[1].itemType,
+      color: p[1].color
+    })
+  })
+
+  console.log(prods)
 
   // Print the html representation of the Catalog JSON
   // The Catalog is a lookup table for the Inventory. To add an item to the inventory, just input the ID + Qty and the properties will be populated
@@ -127,7 +138,7 @@ export default function Catalog() {
   return (
     <div className="App">
       <CatalogTable
-        products={Object.entries(catalog)}
+        products={prods}
       />
     </div>
   );
